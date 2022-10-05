60b18f104e1ffa9b8719c06670846611ac81af39
Anvar Khami <Xamiyev_enver@mail.ru>
2022-10-05 21:18:15 +0400

    s

--- Resant/js/index.js
+++ Resant/js/index.js
@@ -189,7 +189,7 @@ $(function () {
                 mapLatLng.push([Number($(".mapRestaurantItem")[mapListItem].getAttribute('lat')), Number($(".mapRestaurantItem")[mapListItem].getAttribute('lng')), $(".mapRestaurantItem")[mapListItem].getAttribute('name')]);
             });
 
-            function addMarker(late, lange, labelText) {
+            function addMarker(late, lange) {
                 // const svgMarker = {
                 //     path: "M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z",
                 //     fillColor: "#EE3535",
@@ -202,8 +202,7 @@ $(function () {
                 marker = new google.maps.Marker({
                     map: map,
                     position: new google.maps.LatLng(late, lange),
-                    animation: google.maps.Animation.DROP,
-                    label: { color: '#555', fontWeight: '900', fontSize: '14px', text: labelText, className: 'marker-position' }
+                    animation: google.maps.Animation.DROP
                 });
             }
             for (var i = 0; i < mapLatLng.length; i++) {
