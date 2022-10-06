63bd894760f5d40c0b42156819b31fe2828e2c19
Anvar Khami <Xamiyev_enver@mail.ru>
2022-10-06 18:15:53 +0400

    s

--- Resant/js/index.js
+++ Resant/js/index.js
@@ -106,11 +106,17 @@ $(function () {
 
     // hotels slider
     // event owl carousel
+    const chevronLeft = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>';
+    const chevronRight = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>';
     if ($('.hotelsSliderCover').length > 0) {
         $('.hotelsSliderCover').owlCarousel({
             loop: true,
             margin: 32,
-            nav: false,
+            nav: true,
+            navText: [
+                chevronLeft,
+                chevronRight
+            ],
             dots: false,
             smartSpeed: 3000, //smooth effect
             autoplay: true, //smooth autoplya effect ><
@@ -282,8 +288,9 @@ $(function () {
 })
 
 // Page onload - loading
-window.onload = (e) => {
-    document.getElementsByClassName("onloadPage")[0].classList.add("onloadPageNone");
+window.onload = () => {
+    document.getElementsByClassName("onloadResant")[0].classList.add("onloadPageNone");
 }
+
 
 
