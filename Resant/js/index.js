308833226f8d48643f1b1829e6930fafbe9c5fab
Anvar Khami <Xamiyev_enver@mail.ru>
2022-10-06 21:24:28 +0400

    S

--- Resant/js/index.js
+++ Resant/js/index.js
@@ -285,6 +285,24 @@ $(function () {
         })
     }
 
+    //modal kabinet
+    $(function () {
+        $('a.thumbnail').click(function (e) {
+            e.preventDefault();
+            $('#image-modal .modal-body img').attr('src', $(this).find('img').attr('src'));
+            $("#image-modal").modal('show');
+        });
+        $('#image-modal .modal-body img').on('click', function () {
+            $("#image-modal").modal('hide')
+        });
+    });
+    //kabinet buuton
+
+    $('.restaurantOnePageButton').click(function () {
+        $(this).parents(".restaurantTwoCol-right").toggleClass("restaurantKabinet");
+    })
+
+
 })
 
 // Page onload - loading
