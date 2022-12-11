c9aba46d9d3b2aee8595e26c05b1f165caa05425
Anvar Khami <Xamiyev_enver@mail.ru>
2022-12-11 20:15:01 +0400

    Update index.js

--- Mirror/js/index.js
+++ Mirror/js/index.js
@@ -184,32 +184,32 @@ new fullpage('#fullpage', {
         }
 
         if (destination.index === 3) {
-            // const prizmaName = section.querySelectorAll('.prizmaName');
-            // const prizma_Head1 = section.querySelectorAll('.prizma_Head1');
-            // const prizma_Head2 = section.querySelectorAll('.prizma_Head2');
-            // const prizma_Head3 = section.querySelectorAll('.prizma_Head3');
-            // const prizma_Bottom1 = section.querySelectorAll('.prizma_Bottom1');
-            // const prizma_Bottom2 = section.querySelectorAll('.prizma_Bottom2');
-            // const prizma_Bottom3 = section.querySelectorAll('.prizma_Bottom3');
+            const prizmaName = section.querySelectorAll('.prizmaName');
+            const prizma_Head1 = section.querySelectorAll('.prizma_Head1');
+            const prizma_Head2 = section.querySelectorAll('.prizma_Head2');
+            const prizma_Head3 = section.querySelectorAll('.prizma_Head3');
+            const prizma_Bottom1 = section.querySelectorAll('.prizma_Bottom1');
+            const prizma_Bottom2 = section.querySelectorAll('.prizma_Bottom2');
+            const prizma_Bottom3 = section.querySelectorAll('.prizma_Bottom3');
 
-            // const tlprizma1 = new TimelineMax({ delay: 0.8 });
-            // const tlprizma2 = new TimelineMax({ delay: 0.8 });
-            // const tlprizma6 = new TimelineMax({ delay: 0.8 });
-            // const tlprizma3 = new TimelineMax({ delay: 0.8 });
-            // const tlprizma4 = new TimelineMax({ delay: 0.8 });
-            // const tlprizma5 = new TimelineMax({ delay: 0.8 });
+            const tlprizma1 = new TimelineMax({ delay: 0.8 });
+            const tlprizma2 = new TimelineMax({ delay: 0.8 });
+            const tlprizma6 = new TimelineMax({ delay: 0.8 });
+            const tlprizma3 = new TimelineMax({ delay: 0.8 });
+            const tlprizma4 = new TimelineMax({ delay: 0.8 });
+            const tlprizma5 = new TimelineMax({ delay: 0.8 });
 
-            // const tlprizmaname1 = new TimelineMax({ delay: 0.5 });
+            const tlprizmaname1 = new TimelineMax({ delay: 0.5 });
 
-            // tlprizma1.fromTo(prizma_Head1, 1.4, { x: "-2000" }, { x: 0 });
-            // tlprizma2.fromTo(prizma_Head2, 1.6, { y: "-2000" }, { y: 0 });
-            // tlprizma6.fromTo(prizma_Head3, 1.5, { x: "2000" }, { x: 0 });
+            tlprizma1.fromTo(prizma_Head1, 1.4, { x: "-2000" }, { x: 0 });
+            tlprizma2.fromTo(prizma_Head2, 1.6, { y: "-2000" }, { y: 0 });
+            tlprizma6.fromTo(prizma_Head3, 1.5, { x: "2000" }, { x: 0 });
 
-            // tlprizma3.fromTo(prizma_Bottom1, 1.5, { x: "-2000" }, { x: 0 });
-            // tlprizma4.fromTo(prizma_Bottom2, 1.6, { y: "2000" }, { y: 0 });
-            // tlprizma5.fromTo(prizma_Bottom3, 1.4, { x: "2000" }, { x: 0 });
+            tlprizma3.fromTo(prizma_Bottom1, 1.5, { x: "-2000" }, { x: 0 });
+            tlprizma4.fromTo(prizma_Bottom2, 1.6, { y: "2000" }, { y: 0 });
+            tlprizma5.fromTo(prizma_Bottom3, 1.4, { x: "2000" }, { x: 0 });
 
-            // tlprizmaname1.fromTo(prizmaName, 1.3, { scale: 0 }, { scale: 1 });
+            tlprizmaname1.fromTo(prizmaName, 1.3, { scale: 0 }, { scale: 1 });
 
         }
 
