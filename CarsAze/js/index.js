61699bd94c23d49839ab12f213b12431df9c69be
Anvar Khamiyev <54187515+KhamiyevAnvar@users.noreply.github.com>
2023-11-02 01:28:46 +0400

    cars

---
+++ CarsAze/js/index.js
@@ -0,0 +1,90 @@
+$(function () {
+  // menu but
+  var menuBut = $(".carsMenuBut");
+  var menuCloseBut = $(".mobileCloseBut");
+  var mobileMenu = $(".mobileMenu");
+
+  menuBut.click(function () {
+    mobileMenu.removeClass("mobileMenuNone");
+  });
+  menuCloseBut.click(function () {
+    mobileMenu.addClass("mobileMenuNone");
+  });
+
+  // partners carousel
+  if ($(".headCover").length > 0) {
+    $(".headCover").owlCarousel({
+      // rtl: true,
+      loop: true,
+      margin: 15,
+      nav: false,
+      dots: false,
+      smartSpeed: 1500, //smooth effect
+      autoplay: true, //smooth autoplay effect
+      slideTransition: "linear",
+      autoplayTimeout: 6000,
+      autoplaySpeed: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 1,
+        },
+        1020: {
+          items: 1,
+        },
+      },
+    });
+  }
+
+  // partners carousel
+  if ($(".partnersCover").length > 0) {
+    $(".partnersCover").owlCarousel({
+      rtl: true,
+      loop: true,
+      margin: 15,
+      nav: false,
+      dots: false,
+      smartSpeed: 3000, //smooth effect
+      autoplay: true, //smooth autoplay effect
+      slideTransition: "linear",
+      autoplayTimeout: 3000,
+      autoplaySpeed: 1500,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 2,
+        },
+        768: {
+          items: 3,
+        },
+        1020: {
+          items: 4,
+        },
+      },
+    });
+  }
+
+  // contact map
+  if ($("#locationMap").length > 0) {
+    function initMap(getId) {
+      var location = { lat: 40.4004446, lng: 49.8712455 };
+      var map = new google.maps.Map(document.getElementById(getId), {
+        zoom: 16,
+        center: location,
+        zoomControl: true,
+      });
+
+      marker = new google.maps.Marker({
+        map: map,
+        position: location,
+        animation: google.maps.Animation.DROP,
+      });
+    }
+
+    initMap("locationMap");
+    google.maps.event.addDomListener(window, "load", initMap);
+  }
+});
