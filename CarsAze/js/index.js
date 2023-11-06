1a264712b845d76bedc8b3dd1c0039edfa73849b
Anvar Khamiyev <54187515+KhamiyevAnvar@users.noreply.github.com>
2023-11-06 17:51:52 +0400

    cars

--- CarsAze/js/index.js
+++ CarsAze/js/index.js
@@ -149,36 +149,36 @@ $(function () {
         );
         $(".slideshow-text").slick("slickGoTo", nextSlide);
       }
-    })
-    // .on("mousewheel", function (event) {
-    //   event.preventDefault();
-    //   if (event.deltaX > 0 || event.deltaY < 0) {
-    //     $(this).slick("slickNext");
-    //   } else if (event.deltaX < 0 || event.deltaY > 0) {
-    //     $(this).slick("slickPrev");
-    //   }
-    // })
-    .on("mousedown touchstart", function () {
-      dragging = true;
-      tracking = $(".slick-track", $slider).css("transform");
-      tracking = parseInt(tracking.split(",")[5]);
-      rightTracking = $(".slideshow-right .slick-track").css("transform");
-      rightTracking = parseInt(rightTracking.split(",")[5]);
-    })
-    .on("mousemove touchmove", function () {
-      if (dragging) {
-        newTracking = $(".slideshow-left .slick-track").css("transform");
-        newTracking = parseInt(newTracking.split(",")[5]);
-        diffTracking = newTracking - tracking;
-        $(".slideshow-right .slick-track").css({
-          transform:
-            "matrix(1, 0, 0, 1, 0, " + (rightTracking - diffTracking) + ")",
-        });
-      }
-    })
-    .on("mouseleave touchend mouseup", function () {
-      dragging = false;
     });
+  // .on("mousewheel", function (event) {
+  //   event.preventDefault();
+  //   if (event.deltaX > 0 || event.deltaY < 0) {
+  //     $(this).slick("slickNext");
+  //   } else if (event.deltaX < 0 || event.deltaY > 0) {
+  //     $(this).slick("slickPrev");
+  //   }
+  // })
+  // .on("mousedown touchstart", function () {
+  //   dragging = true;
+  //   tracking = $(".slick-track", $slider).css("transform");
+  //   tracking = parseInt(tracking.split(",")[5]);
+  //   rightTracking = $(".slideshow-right .slick-track").css("transform");
+  //   rightTracking = parseInt(rightTracking.split(",")[5]);
+  // })
+  // .on("mousemove touchmove", function () {
+  //   if (dragging) {
+  //     newTracking = $(".slideshow-left .slick-track").css("transform");
+  //     newTracking = parseInt(newTracking.split(",")[5]);
+  //     diffTracking = newTracking - tracking;
+  //     $(".slideshow-right .slick-track").css({
+  //       transform:
+  //         "matrix(1, 0, 0, 1, 0, " + (rightTracking - diffTracking) + ")",
+  //     });
+  //   }
+  // })
+  // .on("mouseleave touchend mouseup", function () {
+  //   dragging = false;
+  // });
 
   $(".slideshow-right .slider").slick({
     swipe: false,
