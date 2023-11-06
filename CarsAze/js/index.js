ae6c6721ea4a966394d99ea9f18ace474c591830
Anvar Khamiyev <54187515+KhamiyevAnvar@users.noreply.github.com>
2023-11-06 19:31:17 +0400

    cars

--- CarsAze/js/index.js
+++ CarsAze/js/index.js
@@ -95,51 +95,23 @@ $(function () {
   }
 });
 
-$(document).ready(function () {
-  var slide = $(".slide");
-  var viewWidth = $(window).width();
-  var sliderInner = $(".slider-inner");
-  var childrenNo = sliderInner.children().length;
-
-  sliderInner.width(viewWidth * childrenNo);
-
-  $(window).resize(function () {
-    viewWidth = $(window).width();
-  });
-
-  function setWidth() {
-    slide.each(function () {
-      $(this).width(viewWidth);
-      $(this).css("left", viewWidth * $(this).index());
-    });
-  }
-
-  function setActive(element) {
-    var clickedIndex = element.index();
-
-    $(".slider-nav .active").removeClass("active");
-    element.addClass("active");
-
-    sliderInner.css(
-      "transform",
-      "translateX(-" + clickedIndex * viewWidth + "px) translateZ(0)"
-    );
-
-    $(".slider-inner .active").removeClass("active");
-    $(".slider-inner .slide").eq(clickedIndex).addClass("active");
-  }
-
-  setWidth();
-
-  $(".slider-nav > div").on("click", function () {
-    setActive($(this));
-  });
-
-  $(window).resize(function () {
-    setWidth();
-  });
-
-  setTimeout(function () {
-    $(".slider").fadeIn(500);
-  }, 2000);
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: true,
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  autoplay: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
 });
